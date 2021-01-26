bf8b960501db8ba692d2cabe9f734f42ae26131a
mohammad sadique <mdsadique007100@gmail.com>
2021-01-26 16:47:10 +0530

    publisher are added and extra function in composer.

--- src/PrivilegeServiceProvider.php
+++ src/PrivilegeServiceProvider.php
@@ -11,6 +11,10 @@ class PrivilegeServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/resources/views', 'privilege');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+
+        $this->publishes([
+            __DIR__.'/resources/views' => resource_path('views/vendor/privilege'),
+        ]);
     }
 
     public function register()
